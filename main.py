3822c3d49a301573f10ac8c20eded43fc88585b4
ritage1995 <amal.hmidani95@gmail.com>
2024-08-02 11:34:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from Voiture import *
 tstClass1 = MyFirstClass("Emey")
 tstClass1.HelloMe()
 
-voiture_bleu = Voiture("bleu")
+voiture_bleu = Voiture()
+voiture_bleu = "bleu"
 print(voiture_bleu.couleur)
