08ffc99053d6992266d54f2779c99dff2d6f518b
Emey Hm <amal.hmidani95@gmail.com>
2024-08-02 11:57:43 +0100

    Update Voiture.py

--- Voiture.py
+++ Voiture.py
@@ -1,5 +1,15 @@
 class Voiture:
   
+  #constructeur
   def __init__(self):
     self.couleur = ""
+    self.__prix = ""
+
+  #getter
+  def getPrix(self):
+    return self.__prix
+
+  #setter
+  def setPrix(self,prix):
+    slef.__prix = prix
 
