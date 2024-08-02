62aeb07d1cd70cbfe4a46e4b827d12c1d5842f34
Emey Hm <amal.hmidani95@gmail.com>
2024-08-02 11:53:56 +0100

    Update MyFirstClass.py

--- MyFirstClass.py
+++ MyFirstClass.py
@@ -1,9 +1,12 @@
 # my hello class 
 class MyFirstClass:
   
+  #constructeur
   def __init__(self,name):
     self.name = name
-    
+
+  #method
   def HelloMe(self):
     print(f"Hello {self.name}")
-    
+
+
